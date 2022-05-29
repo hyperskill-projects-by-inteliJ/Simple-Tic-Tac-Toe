5865d34598453bf4b28ed00ade8fda65bb514688
Manoj R. Randeni <randeni.ramr@gmail.com>
2022-05-29 17:39:36 +0530

    create structure for tic-tac-toe

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,10 +1,45 @@
 package tictactoe;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
-        System.out.println("XOX");
-        System.out.println("OXO");
-        System.out.println("XXO");
+public class Main {
+
+    static String cellInput = "";
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter cells: ");
+        cellInput = scanner.nextLine();
+
+        createCell();
+    }
+
+    public static void createCell() {
+        printBoundaries();
+
+        int cellInputLength = cellInput.length();
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < cellInputLength; i++) {
+            if (i % 3 == 0) {
+                sb.append("| ").append(cellInput.charAt(i)).append(" ");
+            } else if ((i + 1) % 3 == 0) {
+                sb.append(cellInput.charAt(i)).append(" ").append("|");
+            } else {
+                sb.append(cellInput.charAt(i)).append(" ");
+            }
+
+            if (i < cellInputLength - 1 && (i + 1) % 3 == 0) {
+                sb.append("\n");
+            }
+        }
+        System.out.println(sb);
+
+        printBoundaries();
+    }
+
+    public static void printBoundaries() {
+        System.out.println("---------");
     }
 }
