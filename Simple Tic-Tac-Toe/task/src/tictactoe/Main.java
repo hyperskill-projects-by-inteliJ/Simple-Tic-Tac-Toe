8701d024da8e2f48e3472e36c96f528079f91cf9
Manoj R. Randeni <randeni.ramr@gmail.com>
2022-05-29 16:54:06 +0530

    setting the stage for the game

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -2,6 +2,9 @@ package tictactoe;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+
+        System.out.println("XOX");
+        System.out.println("OXO");
+        System.out.println("XXO");
     }
 }
